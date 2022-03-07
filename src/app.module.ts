e884b00064a65eb2eb0f2054a3f005334d04a185
Amit Zulan <amitzulan@gmail.com>
2022-03-07 17:17:56 +0200

    university mostly fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UniversityController } from './university/university.controller';
+import { UniversityService } from './university/university.service';
+import { UniversityModule } from './university/university.module';
 import { StudentController } from './student/student.controller';
 import { EnrollController } from './enroll/enroll.controller';
-import { UniversityService } from './university/university.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [UniversityModule, MongooseModule.forRoot('mongodb://localhost:27017/Universities')],
   controllers: [AppController, UniversityController, StudentController, EnrollController],
   providers: [AppService, UniversityService],
 })
--- src/university/university.service.ts
+++ src/university/university.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { University } from './interfaces/university.interface';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UniversityService {}
+export class UniversityService {
+    constructor(@InjectModel('University') private readonly universityModel: Model<University>) {}
+
+    async find (id: string): Promise<University> {
+        return await this.universityModel.findOne({ _id: id});
+    }
+
+    async create (university: University): Promise<University> {
+        const newUniversity = new this.universityModel(university);
+        return await newUniversity.save();
+    }
+}
